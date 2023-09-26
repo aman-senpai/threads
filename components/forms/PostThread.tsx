f244533b423a46d4860d6f00d1acde66157b2eb3
aman-senpai <jemscollin1@gmail.com>
2023-09-26 18:20:18 +0530

    Add organization, community and render it with the community tag thread

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -16,6 +16,7 @@ import {
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { usePathname, useRouter } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
@@ -36,6 +37,7 @@ interface Props {
 const PostThread = ({ userId }: { userId: string }) => {
     const router = useRouter();
     const pathname = usePathname();
+    const { organization } = useOrganization();
 
     const form = useForm({
         resolver: zodResolver(ThreadValidation),
@@ -49,7 +51,7 @@ const PostThread = ({ userId }: { userId: string }) => {
         await createThread({
             text: values.thread,
             author: userId,
-            communityId: null,
+            communityId: organization ? organization.id : null,
             path: pathname,
         });
 
